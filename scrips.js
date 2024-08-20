a509359cb5f7136fa1a953fa9bbb145213922457
Karthika.T <148969362+T-Karthika@users.noreply.github.com>
2024-08-20 22:40:25 +0530

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,8 @@
+window.onscroll = function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+};
